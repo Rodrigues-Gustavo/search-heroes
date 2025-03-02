b2f789251e7a4fcadf3c73bc6b966716012485da
Gustavo <gustavorr001@gmail.com>
2025-03-02 20:43:10 -0300

    ajusta lógica de favoritar personagens e corrige tipo da propriedade onToggleFavorite

--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -1,30 +1,36 @@
 import styles from "./CharacterGrid.module.css";
 import { Character } from "../types/character";
 import heartIcon from "../../public/assets/icones/heart/Path Copy 2.png";
+import filledHeartIcon from "../../public/assets/icones/heart/Path.png";
 
 interface CharacterGridProps {
   characters: Character[];
   onToggleFavorite: (id: number) => void;
+  favoriteIds: number[];
 }
 
-const CharacterGrid = ({ characters, onToggleFavorite }: CharacterGridProps) => {
+const CharacterGrid = ({ characters, onToggleFavorite, favoriteIds }: CharacterGridProps) => {
   return (
     <div className={styles.grid}>
-      {characters.slice(0, 20).map((character) => (
-        <div key={character.id} className={styles.card}>
-          <img 
-            src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
-            alt={character.name} 
-            className={styles.image} 
-          />
-          <div className={styles.info}>
-            <p className={styles.name}>{character.name}</p>
-            <button className={styles.favoriteButton} onClick={() => onToggleFavorite(character.id)}>
-              <img src={heartIcon} alt="Favorito" className={styles.favoriteIcon} />
-            </button>
+      {characters.slice(0, 20).map((character) => {
+        const isFavorite = favoriteIds.includes(character.id);
+
+        return (
+          <div key={character.id} className={styles.card}>
+            <img 
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
+              alt={character.name} 
+              className={styles.image} 
+            />
+            <div className={styles.info}>
+              <p className={styles.name}>{character.name}</p>
+              <button className={styles.favoriteButton} onClick={() => onToggleFavorite(character.id)}>
+                <img src={isFavorite ? filledHeartIcon : heartIcon} alt="Favorito" className={styles.favoriteIcon} />
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
